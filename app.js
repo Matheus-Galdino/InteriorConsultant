2cca93840aab36939e36c375e9052633c0653a2e
Matheus Carvalho <matheus10277@gmail.com>
2020-09-02 18:25:54 -0300

    fixed horizontal scroll, but now i'm able to scroll vertically while on menu

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
+  const wrapper = document.querySelector("body > div");
 
   //Toggle nav
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+    wrapper.classList.toggle("scroll-toggle");
 
     //animate links
     navLinks.forEach((link, index) => {
